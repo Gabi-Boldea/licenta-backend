06774a9afe6447e764b871ab1e1ab4d9dc9f76c7
Gabriel Boldea <gabrielboldea@Gabriels-MacBook-Pro-2023.local>
2024-05-12 04:46:13 +0300

    stripe

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -5,6 +5,12 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 router.get(
+    "/:restaurantId", 
+    param("restarurantId").isString().trim().notEmpty().withMessage("It must be a valid restaurantId"),
+    RestaurantController.getRestaurant
+)
+
+router.get(
     "/search/:address",
     param("address").isString().trim().notEmpty().withMessage("It must be a valid City"),
     RestaurantController.searchRestaurant
